b62d055bc9053f05629f6c8946125dc103163368
hibuddies <odpazi04973@gmail.com>
2021-02-04 16:14:55 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -28,10 +28,10 @@ git clone https://github.com/lisaac/luci-app-dockerman.git package/luci-app-dock
 git clone https://github.com/rufengsuixing/luci-app-autoipsetadder.git package/luci-app-autoipsetadder
 git clone https://github.com/mchome/openwrt-dogcom.git package/openwrt-dogcom
 
-git clone https://github.com/vernesong/OpenClash.git package/OpenClash
-cp -r package/OpenClash/luci-app-openclash package/
-rm -rf package/OpenClash
-#svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
+#git clone https://github.com/vernesong/OpenClash.git package/OpenClash
+#cp -r package/OpenClash/luci-app-openclash package/
+#rm -rf package/OpenClash
+svn co https://github.com/vernesong/OpenClash/trunk/luci-app-openclash package/luci-app-openclash
 # 编译 po2lmo (如果有po2lmo可跳过)
 pushd package/luci-app-openclash/tools/po2lmo
 make && sudo make install
@@ -46,9 +46,8 @@ svn co https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw/filebrows
 svn co https://github.com/project-openwrt/openwrt/trunk/package/lienol/luci-app-fileassistant package/luci-app-fileassistant
 svn co https://github.com/xiaorouji/openwrt-passwall/trunk/luci-app-passwall package/luci-app-passwall
 svn co https://github.com/xiaorouji/openwrt-passwall/trunk/ssocks package/ssocks
-#svn co https://github.com/xiaorouji/openwrt-passwall/trunk/xray package/xray
-svn co https://github.com/xiaorouji/openwrt-passwall/trunk/xray-core package/xray-core
-svn co https://github.com/fw876/helloworld/trunk/xray package/xray
+#svn co https://github.com/xiaorouji/openwrt-passwall/trunk/xray-core package/xray-core
+svn co https://github.com/fw876/helloworld/trunk/xray-core package/xray-core
 #svn co https://github.com/kenzok8/openwrt-packages/trunk/luci-app-gost package/luci-app-gost
 #svn co https://github.com/kenzok8/openwrt-packages/trunk/gost package/gost
 svn co https://github.com/project-openwrt/openwrt/trunk/package/ctcgfw/luci-app-gost package/luci-app-gost
